5891f397311a4f92d82d8a296518d3be89d20009
Dharmendra Shah <dharmendraa.sah@gmail.com>
2023-03-20 10:14:00 +0545

    first commit

---
+++ app/src/main/java/com/example/tipcalculater/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.tipcalculater
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tipcalculater.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
